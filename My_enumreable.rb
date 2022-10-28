bbff3b41ff891a84b8c1a849549d016bf97d34fe
Anas <anas.chaabane98@gmail.com>
2022-10-28 21:46:41 +0100

    Mylist

---
+++ My_enumreable.rb
@@ -0,0 +1,24 @@
+module MyEnumerable
+  def myList
+    @list ||= []
+  end
+
+
+  def all?
+    verif = []
+    myList.each {|e| verif.push(e) if yield(e) }
+    puts myList.length == verif.length
+  end
+
+  def any?
+    verif = []
+    myList.each {|e| verif.push(e) if yield(e)}
+    puts !verif.empty?
+  end
+
+  def filter? 
+    verif = []
+    myList.each {|e| verif.push(e) if yield(e)}
+    print verif
+  end
+end
---
+++ My_list.rb
@@ -0,0 +1,19 @@
+require './My_enumreable'
+include MyEnumerable
+class Mylist
+  def initialize(*list)
+    @list =  list
+  end
+
+end 
+
+list = Mylist.new(1, 2, 3, 4)
+list.all? { |e| e < 5 } # => true
+list.all? { |e| e > 5 } # => false
+
+# This is for any method
+list.any? { |e| e == 2 } # => true
+list.any? { |e| e == 5 } # => false
+
+# This is for filter method
+list.filter?(&:even?) # [2, 4]
